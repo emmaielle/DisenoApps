b870e1e2445c23194e75f5f074bc830aa5be923b
emmaielle <moira.lasserre@gmail.com>
2016-05-03 21:38:55 -0300

    2016.05.03 versiones arreglosArchivos

--- Obligatorio DDA20160503/ObligatorioDDA2016/src/controlador/ControladorJuegos.java
+++ Obligatorio DDA20160503/ObligatorioDDA2016/src/controlador/ControladorJuegos.java
@@ -19,10 +19,10 @@ import vista.VistaJuegosV1;
 public class ControladorJuegos implements Observer {
 
     private Modelo modelo = Modelo.getInstancia();
-    private VistaJuegosV1 vista;
+    private VistaJuegos vista;
     private Jugador jugador;
     
-    public ControladorJuegos(VistaJuegosV1 v ,Jugador j){
+    public ControladorJuegos(VistaJuegos v ,Jugador j){
         this.vista = v;
         modelo.addObserver(this);
         jugador = j;
